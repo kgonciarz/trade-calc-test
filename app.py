e081516136655ed30c3a3c66a2a7d52f432b3c44
kgonciarz <kgonciarz@cocoasource.ch>
2025-07-30 18:43:25 +0200

    Update app.py

--- app.py
+++ app.py
@@ -57,12 +57,12 @@ calc_type = st.sidebar.selectbox(
 is_reverse = calc_type == "Margin Calculation"
 
 if is_reverse:
-    target_margin = st.sidebar.number_input(
+    sell_price = st.sidebar.number_input(
         "Target Sell Price (€)", min_value=0.0, value=1400.0, step=10.0
     )
     sell_price = None
 else:
-    sell_price = st.sidebar.number_input(
+    target_margin = st.sidebar.number_input(
         "Target margin (€)", min_value=0.0, value=200.0, step=10.0
     )
     target_margin = None
